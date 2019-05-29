6c355b59dc90ce7998d4c25936353818b437bfdb
tugayevci <tugayevci@gmail.com>
2019-05-29 11:20:39 +0300

    initial commit

---
+++ Vidly/Models/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("name=localConnectionString")
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
+    }
+}
