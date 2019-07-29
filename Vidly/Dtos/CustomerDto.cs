1c1709a03e7967a782c55db2973e22259f748d51
tugayevci <tugayevci@gmail.com>
2019-07-29 21:32:42 +0300

    AutoMapperForCustomerDto

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public int MembershipTypeId { get; set; }
+
+
+        [Min18YearsIfAMember]
+        [Display(Name = "Date of Birth")]
+        public DateTime? Birthdate { get; set; }
+    }
+}
