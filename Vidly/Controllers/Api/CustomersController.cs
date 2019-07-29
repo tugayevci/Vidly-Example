99669da8184c15ea5f1f7b37f77e2536c62e95c6
tugayevci <tugayevci@gmail.com>
2019-07-29 21:16:41 +0300

    CustomerApiController

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private DatabaseContext _context;
+
+        public CustomersController()
+        {
+            _context = new DatabaseContext();
+        }
+
+        // GET /api/customers
+        [HttpGet]
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // GET /api/customers/1
+        [HttpGet]
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(x => x.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if(!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // PUT /api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = _context.Customers.SingleOrDefault(x => x.Id == id);
+
+            if(customerInDb==null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            customerInDb = customer;
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(x => x.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+
+        }
+    }
+}
